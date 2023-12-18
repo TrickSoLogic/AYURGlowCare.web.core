457e57194287c1139bb0335be03185ee6cb980fd
TrickSoLogic <118069084+TrickSoLogic@users.noreply.github.com>
2023-12-18 20:32:11 +0530

    update 78dgas8sd

---
+++ Models/order.cs
@@ -0,0 +1,11 @@
+﻿namespace AYURGlowCare.web.core.Models
+{
+    public class order
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public List<Product> Products { get; set; }
+        public decimal TotalAmount { get; set; }
+    }
+}
