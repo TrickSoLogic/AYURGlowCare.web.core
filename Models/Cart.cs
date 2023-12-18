d0b6c72b576b9094ac87153c7253193959d2994b
TrickSoLogic <118069084+TrickSoLogic@users.noreply.github.com>
2023-12-18 20:33:00 +0530

    update hsd98ya9hsa

---
+++ Models/Cart.cs
@@ -0,0 +1,9 @@
+﻿namespace AYURGlowCare.web.core.Models
+{
+    public class Cart
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public List<Product> Products { get; set; }
+    }
+}
